41cbc4d4edf14b9ff17da05f6cbf450ac75dda46
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-04 23:04:04 +0700

    menambahkan koneksi dan server, res baru nama

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+//koneksi database
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_bengkel'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('MySql Connected');
+});
+
+module.exports = conn;
