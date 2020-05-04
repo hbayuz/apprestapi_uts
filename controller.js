025bf6d063e1df27a01eeb7aa18778bbd41a6949
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-04 23:16:53 +0700

    setting routes di server js

--- controller.js
+++ controller.js
@@ -3,6 +3,6 @@
 var response = require('./res');
 var conn = require('./koneksi');
 
-exports.index = function require(req, res){
+exports.index = function require(req, res) {
     response.ok("Aplikasi REST API Berjalan Mulus~")
 };
--- koneksi.js
+++ koneksi.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 
 //koneksi database
 const conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'db_bengkel'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_bengkel'
 });
 
-conn.connect((err)=>{
-    if(err) throw err;
+conn.connect((err) => {
+    if (err) throw err;
     console.log('MySql Connected');
 });
 
