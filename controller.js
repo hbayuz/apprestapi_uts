5ed9d951901dc2d6a1fd158a8a79dd84a2ac8be9
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-04 23:21:58 +0700

    perbaikan pada controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var conn = require('./koneksi');
 
 exports.index = function require(req, res) {
-    response.ok("Aplikasi REST API Berjalan Mulus~")
+    response.ok("Aplikasi REST API Berjalan Mulus~", res)
 };
