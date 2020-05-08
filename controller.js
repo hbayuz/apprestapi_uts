23f21211bb48e28989ab809afe4aa1e50349126d
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-08 09:46:47 +0700

    berhasil menampilkan semua data montir

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var response = require('./res');
-var conn = require('./koneksi');
+var connection = require('./koneksi');
 
 exports.index = function require(req, res) {
     response.ok("Aplikasi REST API Berjalan Mulus~", res)
+};
+
+//menampilkan semua data montir
+exports.tampilsemuamontir = function(req, res){
+    connection.query('SELECT * FROM t_montir', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
