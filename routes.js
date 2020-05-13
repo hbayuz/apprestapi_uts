3027afbf1f4559514e9fd1b90e1cbed8a6bf9ca4
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-13 21:35:52 +0700

    install package jwt morgan ip

--- routes.js
+++ routes.js
@@ -11,10 +11,13 @@ module.exports = function (app) {
         .get(jsonku.tampilsemuamontir);
     app.route('/tampilmontir/:id')
         .get(jsonku.tampilsemuamontirberdasarkanid);
+
     app.route('/tambahmontir')
         .post(jsonku.tambahMontir);
+
     app.route('/ubahmontir')
         .put(jsonku.ubahMontir);
+        
     app.route('/hapusmontir')
         .delete(jsonku.hapusMontir);
 
@@ -23,10 +26,13 @@ module.exports = function (app) {
         .get(jsonku.tampilsemuasparepart);
     app.route('/tampilsparepart/:id')
         .get(jsonku.tampilsemuasparepartberdasarkanid);
+
     app.route('/tambahsparepart')
         .post(jsonku.tambahSparepart);
+
     app.route('/ubahsparepart')
         .put(jsonku.ubahSparepart);
+
     app.route('/hapussparepart')
         .delete(jsonku.hapusSparepart);
 
